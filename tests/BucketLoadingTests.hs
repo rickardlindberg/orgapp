c52d5a3e6770605982c6079f46971e10b2f7cf70
Rickard Lindberg <ricli85@gmail.com>
2012-01-21 10:44:28 +0100

    Clean up.

--- tests/BucketLoadingTests.hs
+++ tests/BucketLoadingTests.hs
@@ -3,6 +3,7 @@ module BucketLoadingTests (tests) where
 import Bucket (createBucket, loadBucketFrom)
 import qualified Data.Set as Set
 import System.Directory
+import System.FilePath
 import Test.HUnit
 import Utils (withTemporaryDirectory)
 
@@ -11,18 +12,19 @@ tests = test
         filesInBucket <- loadBucketFrom "/a/path"
         assertEqual "Should be empty" [] filesInBucket
 
-    , "return files in bucket" ~: withTemporaryDirectory $ \path -> do
-        givenABucketAt $ path ++ "/bucket"
-        givenFilesInBucketAt $ path ++ "/bucket"
-        filesInBucket <- whenLoadingBucketFrom $ path ++ "/bucket"
+    , "return files in bucket" ~: withTemporaryDirectory $ \tmpDir -> do
+        let bucketPath = tmpDir </> "bucket"
+        givenABucketAt bucketPath
+        givenFilesInBucketAt bucketPath
+        filesInBucket <- whenLoadingBucketFrom bucketPath
         thenOneFileShouldBeReturned filesInBucket
     ]
 
 givenABucketAt = createBucket
 
 givenFilesInBucketAt path = do
-    createDirectory $ path ++ "/oneFile"
-    createDirectory $ path ++ "/anotherFile"
+    createDirectory $ path </> "oneFile"
+    createDirectory $ path </> "anotherFile"
 
 whenLoadingBucketFrom = loadBucketFrom
 
