8311ab09ac8dab70394cc622c1a20e0c1cd17983
Rickard Lindberg <ricli85@gmail.com>
2012-02-04 19:11:58 +0100

    First real interface.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
+import Bucket
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
+import Graphics.UI.Gtk.ModelView.ListStore
+import Graphics.UI.Gtk.ModelView.TreeViewColumn
 
 main = do
     initGUI
@@ -7,11 +10,49 @@ main = do
     builder <- builderNew
     builderAddFromFile builder "interface.glade"
 
-    mainWindow <- builderGetObject builder castToWindow "main_window"
-    onDestroy mainWindow mainQuit
+    mainWindow    <- builderGetObject builder castToWindow   "main_window"
+    importButton  <- builderGetObject builder castToButton   "import_button"
+    searchText    <- builderGetObject builder castToEntry    "search_text"
+    itemsTreeView <- builderGetObject builder castToTreeView "items_tree_view"
 
-    helloWorldButton <- builderGetObject builder castToButton "hello_world_button"
-    onClicked helloWorldButton (putStrLn "Hello World")
+    model <- listStoreNew ([BucketItem "a/path"] :: [BucketItem])
+    initItemsTreeView itemsTreeView model
+
+    mainWindow    `onDestroy`         mainQuit
+    importButton  `onClicked`         handleImportButtonClicked
+    searchText    `onEditableChanged` handleSearchTextChanged searchText
+    itemsTreeView `onRowActivated`    handleItemActivated itemsTreeView model
 
     widgetShowAll mainWindow
     mainGUI
+
+initItemsTreeView :: TreeView -> ListStore BucketItem -> IO ()
+initItemsTreeView treeView model = do
+    treeViewSetModel treeView model
+    createNameColumn model >>= treeViewAppendColumn treeView
+    return ()
+
+createNameColumn :: ListStore BucketItem -> IO TreeViewColumn
+createNameColumn model = do
+    textRenderer <- cellRendererTextNew
+    column       <- treeViewColumnNew
+    treeViewColumnPackStart column textRenderer True
+    cellLayoutSetAttributes column textRenderer model $
+        \item -> [cellText := itemPath item]
+    return column
+
+handleImportButtonClicked = do
+    putStrLn "import clicked"
+
+handleSearchTextChanged searchText = do
+    allText <- editableGetChars searchText 0 (-1)
+    putStrLn $ "serach text changed: " ++ allText
+
+handleItemActivated treeView model treePath treeViewColumn = do
+    item <- getItem treeView model treePath
+    putStrLn $ "activated item: " ++ (itemPath item)
+
+getItem :: TreeView -> ListStore BucketItem -> TreePath -> IO BucketItem
+getItem treeView model treePath = do
+    Just treeIter <- treeModelGetIter model treePath
+    listStoreGetValue model (listStoreIterToIndex treeIter)
