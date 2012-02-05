997173da1c23a6d4dd395d8ad201d6bebe5e581a
Rickard Lindberg <ricli85@gmail.com>
2012-02-05 08:36:05 +0100

    Use FileChooserDialog when importing file.

--- src/MainWindow.hs
+++ src/MainWindow.hs
@@ -1,6 +1,7 @@
 module MainWindow (showMainWindow) where
 
 import Bucket
+import Control.Monad
 import Data.IORef
 import Graphics.UI.Gtk
 import ItemsTreeModel
@@ -9,13 +10,14 @@ showMainWindow :: ItemsTreeModel -> IORef Bucket -> IO ()
 showMainWindow itemsTreeModel currentBucketRef = do
     builder       <- builderFromFile "interface.glade"
 
-    mainWindow    <- builderGetObject builder castToWindow   "main_window"
-    importButton  <- builderGetObject builder castToButton   "import_button"
-    searchText    <- builderGetObject builder castToEntry    "search_text"
-    itemsTreeView <- builderGetObject builder castToTreeView "items_tree_view"
+    mainWindow    <- builderGetObject builder castToWindow            "main_window"
+    importButton  <- builderGetObject builder castToButton            "import_button"
+    searchText    <- builderGetObject builder castToEntry             "search_text"
+    itemsTreeView <- builderGetObject builder castToTreeView          "items_tree_view"
+    fileChooser   <- builderGetObject builder castToFileChooserDialog "import_file_dialog"
 
     mainWindow    `onDestroy`         mainQuit
-    importButton  `onClicked`         handleImportButtonClicked currentBucketRef itemsTreeModel
+    importButton  `onClicked`         handleImportButtonClicked fileChooser currentBucketRef itemsTreeModel
     searchText    `onEditableChanged` handleSearchTextChanged searchText
     itemsTreeView `onRowActivated`    handleItemActivated itemsTreeView itemsTreeModel
 
@@ -44,11 +46,15 @@ createNameColumn model = do
         \item -> [cellText := itemPath item]
     return column
 
-handleImportButtonClicked currentBucketRef itemsTreeModel = do
-    currentBucket <- readIORef currentBucketRef
-    newBucket <- importFile currentBucket "testfile"
-    writeIORef currentBucketRef newBucket
-    updateModel itemsTreeModel newBucket
+handleImportButtonClicked fileChooser currentBucketRef itemsTreeModel = do
+    response <- dialogRun fileChooser
+    when (response == ResponseOk) $ do
+        Just file <- fileChooserGetFilename fileChooser
+        currentBucket <- readIORef currentBucketRef
+        newBucket <- importFile currentBucket file
+        writeIORef currentBucketRef newBucket
+        updateModel itemsTreeModel newBucket
+    widgetHide fileChooser
 
 handleSearchTextChanged searchText = do
     allText <- editableGetChars searchText 0 (-1)
