bba2b06ba85d6fef308deb920fe8e7ac7f81604e
Rickard Lindberg <ricli85@gmail.com>
2012-02-19 14:51:31 +0100

    Inline todo in code.

--- src/SearchFilter.hs
+++ src/SearchFilter.hs
@@ -3,4 +3,6 @@ module SearchFilter (matchSearch) where
 import Bucket
 
 matchSearch :: String -> BucketItem -> Bool
-matchSearch searchString item = True
+matchSearch searchString item =
+    -- TODO: implement smart search
+    True
