d8cc71d43932e4a783d966ecd41bf8c5ee2269a5
Rickard Lindberg <ricli85@gmail.com>
2012-02-26 10:44:27 +0100

    Extract load code to separate file.

--- src/Bucket.hs
+++ src/Bucket.hs
@@ -11,27 +11,5 @@ module Bucket
     ) where
 
 import Bucket.Import
+import Bucket.Load
 import Bucket.Types
-import DirectoryInfo
-import Meta
-import System.Directory
-
-createBucket :: FilePath -> IO Bucket
-createBucket path = do
-    createDirectory path
-    return $ Bucket path []
-
-loadBucketFrom :: FilePath -> IO (Maybe Bucket)
-loadBucketFrom pathToBucket = doesDirectoryExist pathToBucket >>= loadBucketWhenExists
-    where
-        loadBucketWhenExists False = return Nothing
-        loadBucketWhenExists True  = do
-            directories <- getDirectoryInfoRecursive pathToBucket
-            return $ Just $ directoriesToBucket pathToBucket directories
-
-directoriesToBucket :: FilePath -> [DirectoryInfo] -> Bucket
-directoriesToBucket pathToBucket directories = Bucket pathToBucket items
-    where
-        items = directoriesToItems directories
-        directoriesToItems = map directoryToItem . filter hasMetaFile
-        directoryToItem (DirectoryInfo { path = path, meta = Just meta }) = BucketItem path (metaFromStr meta)
