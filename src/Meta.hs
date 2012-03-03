05afbf8594d1e494ad8452a167c9c24bd41701b3
Rickard Lindberg <ricli85@gmail.com>
2012-03-03 16:15:53 +0100

    Add support for multiple values in meta.

--- src/Meta.hs
+++ src/Meta.hs
@@ -5,27 +5,39 @@ import qualified Data.Map as M
 import System.IO
 import Text.ParserCombinators.Parsec
 
-newtype Meta = Meta (M.Map String String)
+newtype Meta = Meta (M.Map String [String])
     deriving (Eq, Show)
 
 createMeta = Meta M.empty
 
 getValue :: String -> String -> Meta -> String
-getValue key defaultValue (Meta m) = fromMaybe defaultValue (key `M.lookup` m)
+getValue key defaultValue meta =
+    case getValues key meta of
+        []  -> defaultValue
+        x:_ -> x
+
+getValues :: String -> Meta -> [String]
+getValues key (Meta m) = fromMaybe [] (key `M.lookup` m)
 
 setValue :: String -> String -> Meta -> Meta
-setValue key value (Meta m) = Meta $ M.insert key value m
+setValue key value (Meta m) = setValues key [value] (Meta m)
+
+setValues :: String -> [String] -> Meta -> Meta
+setValues key values (Meta m) = Meta $ M.insert key values m
 
 writeMeta :: Meta -> FilePath -> IO ()
 writeMeta meta destination = writeFile destination (metaToStr meta)
 
 metaToStr :: Meta -> String
-metaToStr (Meta m) = concatMap entryToLine (M.toList m)
+metaToStr (Meta m) = concat $ concatMap entryToLines (M.toList m)
     where
-        entryToLine (key, value) = key ++ "::" ++ value ++ "\n"
+        entryToLines (key, values) = map (entryToLine key) values
+        entryToLine key value = key ++ "::" ++ value ++ "\n"
 
 metaFromStr :: String -> Meta
-metaFromStr str = Meta $ M.fromList (parseMeta str)
+metaFromStr str = Meta $ M.fromListWith reverseAppend (map (\(a, b) -> (a, [b])) (parseMeta str))
+    where
+        reverseAppend a b = b ++ a
 
 parseMeta :: String -> [(String, String)]
 parseMeta input =
--- tests/TestMeta.hs
+++ tests/TestMeta.hs
@@ -7,14 +7,39 @@ import Test.QuickCheck
 tests = test
     [ "reading meta" ~:
 
-        [ "filename" ~: do
+        [ "single items" ~: do
             let meta = metaFromStr "filename::foo.png\n"
             getValue "filename" "" meta @?= "foo.png"
+
+        , "defaults to given value" ~: do
+            getValue "nonExisting" "DEF" createMeta @?= "DEF"
+
+        , "multiple items" ~: do
+            let meta = metaFromStr "tag::good\ntag::sports\n"
+            getValues "tag" meta @?= ["good", "sports"]
+
+        , "defaults to empty list" ~: do
+            getValues "nonExisting" createMeta @?= []
+
+        , "getting single returns first" ~: do
+            let meta = setValues "tag" ["sports", "good"] $ createMeta
+            getValue "tag" "" meta @?= "sports"
         ]
 
-    , "preserves unkonw fields" ~: do
-        let metaFile = "unknownField::5\n"
-        metaToStr (metaFromStr metaFile) @?= metaFile
+    , "writing meta" ~:
+
+        [ "single items" ~: do
+            let meta = setValue "filename" "foo.png" createMeta
+            metaToStr meta @?= "filename::foo.png\n"
+
+        , "multiple items" ~: do
+            let meta = setValues "tag" ["good", "sports"] $ createMeta
+            metaToStr meta @?= "tag::good\ntag::sports\n"
+
+        , "does not write empty lists" ~: do
+            let meta = setValues "tag" [] $ createMeta
+            metaToStr meta @?= ""
+        ]
 
     , "implementation details" ~:
 
