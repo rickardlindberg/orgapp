ae6b476f0a2edd6ec628f507eb069f54b3cf5308
Rickard Lindberg <ricli85@gmail.com>
2012-03-17 10:53:42 +0100

    refactor: extract tag conversion and remove unnecessary imports

--- src/GUI/TagEditor.hs
+++ src/GUI/TagEditor.hs
@@ -1,33 +1,33 @@
 module GUI.TagEditor where
 
 import Bucket.EditItem
-import Bucket.Import
 import Bucket.Types
 import Control.Monad
 import Data.IORef
 import Data.List
 import Graphics.UI.Gtk
 import GUI.ItemsTreeModel
-import Meta
-import Open
-import SearchFilter
-import System.Directory
 
 handleEditButtonClicked tagEditor treeView model tagEditorText currentBucketRef updateItemList = do
-    -- TODO: extract tags -> comma sep string -> tags somehere else
     (treePath, _) <- treeViewGetCursor treeView
     item <- getItem treeView model treePath
-    entrySetText tagEditorText (intercalate "," (tags item))
+    entrySetText tagEditorText (tagsToString (tags item))
     response <- dialogRun tagEditor
     when (response == ResponseOk) $ do
         currentBucket <- readIORef currentBucketRef
         tagsText <- entryGetText tagEditorText
-        let newItem = setTags item (getTags tagsText)
+        let newItem = setTags item (stringToTags tagsText)
         -- TODO: show error dialog if we can't save item
         newBucket <- editItem currentBucket item newItem
         writeIORef currentBucketRef newBucket
         updateItemList
     widgetHide tagEditor
+
+tagsToString :: [String] -> String
+tagsToString = intercalate ","
+
+stringToTags :: String -> [String]
+stringToTags = getTags
     where
         getTags "" = []
         getTags s = (takeWhile notComma s):getTags (drop 1 (dropWhile notComma s))
