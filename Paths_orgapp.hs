e7dd66252e92d86b14e3429e69d15ec6c43bb78b
Rickard Lindberg <ricli85@gmail.com>
2012-05-04 22:39:55 +0200

    devenv: supply data dir with environment variable instead

--- Paths_orgapp.hs
+++
@@ -1,7 +0,0 @@
--- Override generated paths for dev env:
--- http://neilmitchell.blogspot.se/2008/02/adding-data-files-using-cabal.html
-
-module Paths_orgapp where
-
-getDataFileName :: FilePath -> IO FilePath
-getDataFileName = return
